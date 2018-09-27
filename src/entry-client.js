9f046fb1a221946a8ceb1b9540b24dd1b49aa028
申冬 <dongshen211299@sohu-inc.com>
2018-09-27 18:26:49 +0800

    a global mixin that calls `asyncData` when a route component params change

--- src/entry-client.js
+++ src/entry-client.js
@@ -1,9 +1,25 @@
+import Vue from 'vue'
 import { createApp } from './app';
 const { app, router, store } = createApp();
 
 if (window.__INITIAL_STATE__) {
   store.replaceState(window.__INITIAL_STATE__);
 }
+
+// a global mixin that calls `asyncData` when a route component's params change
+Vue.mixin({
+  beforeRouteUpdate (to, from, next) {
+    const { asyncData } = this.$options
+    if (asyncData) {
+      asyncData({
+        store: this.$store,
+        route: to
+      }).then(next).catch(next)
+    } else {
+      next()
+    }
+  }
+})
 
 router.onReady(() => {
   router.beforeResolve((to, from, next) => {
