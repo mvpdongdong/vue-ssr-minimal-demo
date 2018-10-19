3491d6d7cdfb4691199e0ba838ffcaf632f06a8f
申冬 <dongshen211299@sohu-inc.com>
2018-10-19 18:05:28 +0800

    fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    {{fooCount}}
-    <demo></demo>
     <h3>文章列表</h3>
     <div class="list" v-for="i in list">
       <router-link :to="{path:'/item/'+i.id}">{{i.title}}</router-link>
@@ -9,16 +7,12 @@
   </div>
 </template>
 <script>
-import Demo from '../components/demo';
 import fooStoreModule from '../store/modules/foo';
 
 export default {
   asyncData ({ store, route }) {
     store.registerModule('foo', fooStoreModule);
-    return Promise.all([
-      store.dispatch('foo/inc'),
-      store.dispatch('fetchList')
-    ]);
+    return store.dispatch('fetchList');
   },
   destroyed () {
     this.$store.unregisterModule('foo');
@@ -30,9 +24,6 @@ export default {
     fooCount () {
       return this.$store.state.foo.count;
     }
-  },
-  components: {
-    Demo
   }
 };
 </script>
